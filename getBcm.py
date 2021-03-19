0cbb2ec89c4495c20773d40e4fc1ef4420c4a200
蕉饼 <77569325+BananaCakeComputer@users.noreply.github.com>
2021-03-19 18:24:19 +0800

    Update getBcm.py

--- getBcm.py
+++ getBcm.py
@@ -1,2 +1,4 @@
+'''作者：蕉饼
+这个程序已经不管用了，因为官方修复了这个问题，bcm_url对应空'''
 import requests, webbrowser
 webbrowser.open(requests.get('https://api.codemao.cn/creation-tools/v1/works/' + input('你要获取bcm文件的ID')).json()['bcm_url'])
